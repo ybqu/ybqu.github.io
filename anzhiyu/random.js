bdcb7fca50e0a8385076da7c21fdb46701de3468
yuanbinqu <1830954358@qq.com>
2025-08-31 04:16:27 +0800

    Site updated: 2025-08-31 04:16:27

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["p/70159603.html","p/d516c1d6.html","p/5b4d8163.html","p/635901b3.html"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
